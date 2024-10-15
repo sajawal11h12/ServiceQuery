0042230fb8b5d39f7f76d39748b230af813d65e7
Your Name <you@example.com>
2024-10-14 23:01:30 -0700

    push

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,7 +3,7 @@ import Button from '../../button';
 import ButtonGradient from '../../buttonGrad';
 import headerearth from '../../assets/images/headerearth.json';
 import Lottie from 'lottie-react';
-import BgVector from '../../assets/images/BgVector.webp';
+import BgVector from '../../assets/images/BgVector.png';
 import { Gradient } from '../../Gradient';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -16,33 +16,33 @@ const Heropage = () => {
   }, []);
 
   return (
-    <header
-      className="relative bg-center h-[500px] md:h-[500px]"
-      style={{
-        backgroundImage: `url(${BgVector})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center 20%', // Adjust this value to move it down
-      }}
-    >
-      <div className=" pt-32 flex flex-col md:flex-row items-center  justify-center h-full text-white">
-      <div 
-  className="  ml-28 md:w-1/2 flex flex-col  mt-12 text-left md:pr-8" 
-  data-aos="fade-up" // AOS animation for fade-up
->
-  <h1 className="text-lg font-bold  text-gray-50 md:text-6xl lg:text-5xl mb-4">
-    Revolutionize Data Access 
-    <span className="gradi-theme-text block text-[#00FFCC] text-6xl lg:text-[50px]">
-    with Query Service
-    </span>
-  </h1>
-  <p className="mx-auto lg:max-w-[600px] lg:text-lg md:text-base text-sm text-gray-300 my-6 leading-relaxed">
-  The open-source library for efficient data querying over REST APIs facilitates seamless integration with various SQL and NoSQL databases. It supports polyglot, multi-engine database access, allowing developers to perform secure and flexible queries tailored to their specific needs.
-  </p>
-
-
-
-
-        
+    <header className="relative bg-center max-h-[80%] w-[110%]">
+      <div
+        className=" absolute inset-0 opacity-70" // Add Tailwind opacity utility here
+        style={{
+          backgroundImage: `url(${BgVector})`,
+          backgroundSize: 'cover',
+          backgroundPosition: '30%'
+          ,
+           // Adjust this value to move it down
+        }}
+      ></div>
+      
+      <div className="  relative pt-32 flex flex-col md:flex-row items-center justify-center h-full text-white">
+        <div 
+          className="mb-64 ml-28 md:w-1/2 flex flex-col mt-12 text-left md:pr-8" 
+          data-aos="fade-up" // AOS animation for fade-up
+        >
+          <h1 className="text-lg font-bold ml-7 text-gray-50 md:text-6xl lg:text-5xl mb-4">
+            Revolutionize Data Access 
+            <span className="gradi-theme-text block text-[#00FFCC] text-6xl lg:text-[50px]">
+              with Query Service
+            </span>
+          </h1>
+          <p className="mx-auto lg:max-w-[600px] lg:text-lg md:text-base text-sm text-white my-6 leading-relaxed">
+            The open-source library for efficient data querying over REST APIs facilitates seamless integration with various SQL and NoSQL databases. It supports polyglot, multi-engine database access, allowing developers to perform secure and flexible queries tailored to their specific needs.
+          </p>
+          
           <form className="w-full mt-4">
             <div className="flex flex-col justify-start md:flex-row md:max-w-[610px] mx-auto space-y-4 md:space-y-0 md:space-x-4">
               <Gradient /> {/* Added Gradient component here */}
@@ -59,18 +59,12 @@ const Heropage = () => {
         >
           <Lottie
             animationData={headerearth}
-            className="mt-8 w-full h-[400px] md:h-[450px] object-cover rounded-lg shadow-lg"
+            className="mb-48 mr-40 w-full h-[400px] md:h-[450px] object-cover rounded-lg shadow-lg"
             loop={true}
             autoplay={true}
           />
         </div>
       </div>
-      <Lottie
-            animationData={Arrow}
-            className="mt-16 w-full h-[50px] md:h-[50px] object-cover rounded-lg shadow-lg"
-            loop={true}
-            autoplay={true}
-          />
 
     </header>
   );
