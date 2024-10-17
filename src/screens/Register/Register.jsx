92d2c2113e425138e60b37667570367a6da5fb40
Your Name <you@example.com>
2024-10-17 04:16:52 -0700

    ok

--- src/screens/Register/Register.jsx
+++ src/screens/Register/Register.jsx
@@ -7,7 +7,7 @@ import ButtonGradient from '../../buttonGrad';
 import { useForm } from 'react-hook-form';
 import Header from '../../layout/Header';
 import Footer from '../../layout/Footer';
-import './styles.css'
+
 
 const Register = () => {
   const {
